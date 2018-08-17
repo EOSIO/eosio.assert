83ea9e17c9bd1a75e0f21cbd124be9200b68dec1
Todd Fleming <todd.fleming@block.one>
2018-08-17 16:43:14 -0400

    eosio.assert

--- tests/eosio_assert_tests.cpp
+++ tests/eosio_assert_tests.cpp
@@ -22,6 +22,8 @@ using namespace eosio::chain;
 using namespace eosio::testing;
 using namespace fc;
 
+inline constexpr auto operator""_n(const char* s, std::size_t) { return string_to_name(s); }
+
 #define CHECK_ASSERT(S, M)                                                                                             \
    try {                                                                                                               \
       S;                                                                                                               \
@@ -66,21 +68,68 @@ class assert_tester : public TESTER {
        : TESTER(), test_name{test_name}, outfile{DATA_DIR + test_name + ".actual"}, abi{contracts::eosio_assert_abi()},
          abi_ser(json::from_string(std::string{abi.data(), abi.data() + abi.size()}).as<abi_def>(),
                  abi_serializer_max_time) {
-      create_account(N(eosio.assert));
-      set_code(N(eosio.assert), contracts::eosio_assert_wasm());
+      create_account("eosio.assert"_n);
+      set_code("eosio.assert"_n, contracts::eosio_assert_wasm());
    }
 
-   void dump_table(name account, name scope, name table) {
-      outfile << account << "." << scope << "." << table << "\n";
-      const auto& db  = control->db();
-      const auto* tbl = db.find<table_id_object, by_code_scope_table>(boost::make_tuple(account, scope, table));
+   struct row {
+      uint64_t primary_key;
+      bytes    value;
+   };
+
+   auto get_table(name account, name scope, name table) {
+      std::vector<row> rows;
+      const auto&      db  = control->db();
+      const auto*      tbl = db.find<table_id_object, by_code_scope_table>(boost::make_tuple(account, scope, table));
       if (!tbl)
-         return;
+         return rows;
       auto& idx = db.get_index<key_value_index, by_scope_primary>();
-      for (auto it = idx.lower_bound(std::make_tuple(tbl->id, 0)); it != idx.end() && it->t_id == tbl->id; ++it) {
-         outfile << "...\n";
-      }
+      for (auto it = idx.lower_bound(std::make_tuple(tbl->id, 0)); it != idx.end() && it->t_id == tbl->id; ++it)
+         rows.push_back(row{it->primary_key, bytes{it->value.begin(), it->value.end()}});
+      return rows;
    }
+
+   void diff_table(name account, name scope, name table, const std::string& type, std::vector<row>& existing) {
+      outfile << "table: " << account << " " << scope << " " << table << "\n";
+      auto updated = get_table(account, scope, table);
+
+      std::map<uint64_t, std::pair<optional<bytes>, optional<bytes>>> comparison;
+      for (auto& x : existing)
+         comparison[x.primary_key].first = x.value;
+      for (auto& x : updated)
+         comparison[x.primary_key].second = x.value;
+
+      auto str = [&](bytes& b) {
+         auto        s = "\n" + json::to_pretty_string(abi_ser.binary_to_variant(type, b, abi_serializer_max_time));
+         std::string result;
+         for (auto ch : s)
+            if (ch == '\n')
+               result += "\n        ";
+            else
+               result += ch;
+         return result;
+      };
+
+      for (auto& x : comparison) {
+         if (!x.second.first)
+            outfile << "    add row:" << str(*x.second.second) << "\n";
+         else if (!x.second.second)
+            outfile << "    del row:" << str(*x.second.first) << "\n";
+         else if (*x.second.first != *x.second.second)
+            outfile << "    change row:" << str(*x.second.second) << "\n";
+      }
+      existing = std::move(updated);
+   }
+
+   struct table {
+      name             account;
+      name             scope;
+      name             table;
+      std::string      type;
+      std::vector<row> rows;
+   };
+
+   void diff_table(table& t) { diff_table(t.account, t.scope, t.table, t.type, t.rows); }
 
    void heading(const std::string& s) { outfile << "\n" << s << "\n=========================\n"; }
 
@@ -103,12 +152,13 @@ class assert_tester : public TESTER {
 BOOST_AUTO_TEST_SUITE(eosio_assert)
 
 BOOST_AUTO_TEST_CASE(bootstrap) try {
-   assert_tester t{"bootstrap"};
-   t.create_account(N(dapp1));
-   t.create_account(N(dapp2));
+   assert_tester        t{"bootstrap"};
+   assert_tester::table manifests{"eosio.assert"_n, "eosio.assert"_n, "manifests"_n, "stored_manifest"};
+   t.create_account("dapp1"_n);
+   t.create_account("dapp2"_n);
 
    t.heading("add.manifest: missing authority");
-   t.push_transaction(N(dapp2), R"({
+   t.push_transaction("dapp2"_n, R"({
       "actions": [{
          "account":              "eosio.assert",
          "name":                 "add.manifest",
@@ -133,7 +183,7 @@ BOOST_AUTO_TEST_CASE(bootstrap) try {
    })");
 
    t.heading("add.manifest");
-   t.push_transaction(N(dapp1), R"({
+   t.push_transaction("dapp1"_n, R"({
       "actions": [{
          "account":              "eosio.assert",
          "name":                 "add.manifest",
@@ -156,10 +206,11 @@ BOOST_AUTO_TEST_CASE(bootstrap) try {
          },
       }]
    })");
+   t.diff_table(manifests);
 
    t.heading("add.manifest: duplicate");
    t.produce_block();
-   t.push_transaction(N(dapp1), R"({
+   t.push_transaction("dapp1"_n, R"({
       "actions": [{
          "account":              "eosio.assert",
          "name":                 "add.manifest",
